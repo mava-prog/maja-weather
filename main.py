bdd41207cd97afd7661df7ffe922f543202ba757
Maja Valentincic <maja.valentincic@me.com>
2020-01-13 18:21:04 +0100

    prva verzija

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+import requests
+from random import  randint
+
+app = Flask(__name__)
+
+API_KEY = open("API_KEY.txt").read().strip()
+
+
+
+@app.route("/", methods=["GET"])
+def index():
+    ime_mesta = ["Ljubljana", "London", "Paris"] [randint (0,2)]
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={ime_mesta}&appid={API_KEY}&units=metric"
+    result =requests.get(URL).json()
+    return render_template("index.html", data =result)
+
+if __name__=="__main__":
+    app.run()
